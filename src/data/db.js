b0e4a1675f3e9de04f38df53ac23ae07839d04e1
CheoR <cheo.roman+github@gmail.com>
2022-08-18 11:20:50 -0500

    fix: format to conform prettier rules

--- src/data/db.js
+++ src/data/db.js
@@ -1,26 +1,26 @@
 const users = [
   {
-    _id: "53a0724c-a416-4cac-ae45-bfaedce1f147",
-    username: "user1",
+    _id: '53a0724c-a416-4cac-ae45-bfaedce1f147',
+    username: 'user1',
   },
   {
-    _id: "c2af9adc-d0b8-4d44-871f-cef66f86f7f6",
-    username: "user2",
+    _id: 'c2af9adc-d0b8-4d44-871f-cef66f86f7f6',
+    username: 'user2',
   },
   {
-    _id: "2c931e7e-510f-49e5-aed6-d6b44087e5a1",
-    username: "user3",
+    _id: '2c931e7e-510f-49e5-aed6-d6b44087e5a1',
+    username: 'user3',
   },
   {
-    _id: "404daf2a-9b97-4b99-b9af-614d07f818d7",
-    username: "user4",
+    _id: '404daf2a-9b97-4b99-b9af-614d07f818d7',
+    username: 'user4',
   },
   {
-    _id: "6379c436-9fad-4b3f-a427-2d7241f5c1b1",
-    username: "user5",
+    _id: '6379c436-9fad-4b3f-a427-2d7241f5c1b1',
+    username: 'user5',
   },
-];
+]
 
 exports.db = {
   users,
-};
+}
