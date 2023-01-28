7ecd91e9ce6bfa5be49efc5212ee7b7784d4c8d2
CheoR <cheo.roman+github@gmail.com>
2023-01-28 15:14:14 -0600

    build: Box model and input type

---
+++ src/types/box.ts
@@ -0,0 +1,40 @@
+import { Document, Types } from 'mongoose'
+
+// TODO: check for updates
+// https://github.com/GraphQLGuide/apollo-datasource-mongodb/issues/78
+export interface IBox extends Document {
+  description: string
+  move_id: Types.ObjectId
+  name: string
+  user_id: Types.ObjectId
+}
+
+export interface IBoxInput {
+  input: {
+    count: number
+    description: string
+    move_id: Types.ObjectId
+    name: string
+    user_id: Types.ObjectId
+  }
+}
+
+export type BoxError = {
+  message: string | ErrorMessages
+  resolution?: string | undefined
+}
+
+export const enum ErrorMessages {
+  DBError = 'Error occurred while dealing with db',
+  ServerError = 'Error occurred while dealing with db',
+  CreateError = 'Could not add box',
+  ReadError = 'Could not fetch box',
+  UpdateError = 'Could not update box',
+  DeleteError = 'Could not remove box',
+}
+
+export const isError = (
+  toBeDetermined: unknown | BoxError,
+): toBeDetermined is BoxError => {
+  return !!(toBeDetermined as BoxError)?.message
+}
