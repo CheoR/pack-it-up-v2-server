f23dad1dc794fc6caeee8805346c61942be84e81
CheoR <cheo.roman+github@gmail.com>
2023-01-27 21:30:05 -0600

    build: wire up Item datasource to server

--- src/server.ts
+++ src/server.ts
@@ -12,11 +12,13 @@ import http from 'http'
 import { RefreshToken as TokenModel } from './models/refreshToken'
 import { User as UserModel } from './models/user'
 import { Move as MoveModel } from './models/move'
+import { Item as ItemModel } from './models/item'
 import { Mutation } from './resolvers/Mutation'
 import { typeDefs } from './schemas/typeDefs'
 import TokensAPI from './dataSources/Tokens'
 import MovesAPI from './dataSources/Moves'
 import UsersAPI from './dataSources/Users'
+import ItemsAPI from './dataSources/Items'
 import { Query } from './resolvers/Query'
 import connectDB from './config/db'
 import {
@@ -28,6 +30,7 @@ import {
 export interface AppContext {
   token?: string | undefined
   dataSources?: {
+    itemsAPI: ItemsAPI
     movesAPI: MovesAPI
     tokensAPI: TokensAPI
     usersAPI: UsersAPI
@@ -127,6 +130,7 @@ async function startApolloServer() {
           req,
           user_id: user_id ?? null,
           dataSources: {
+            itemsAPI: new ItemsAPI({ collection: ItemModel, cache }),
             movesAPI: new MovesAPI({ collection: MoveModel, cache }),
             tokensAPI: new TokensAPI({ collection: TokenModel, cache }),
             usersAPI: new UsersAPI({ collection: UserModel, cache }),
