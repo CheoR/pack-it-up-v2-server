f1600f9c245f6e5d7dba585cd1e7700cf8d6b86f
CheoR <cheo.roman+github@gmail.com>
2023-03-21 10:12:53 -0500

    config: cloudinary

---
+++ src/config/cloudinary.ts
@@ -0,0 +1,7 @@
+const config = {
+  api_key: `${process.env.CLOUDINARY_API_KEY}`,
+  api_secret: `${process.env.CLOUDINARY_API_SECRET}`,
+  cloud_name: `${process.env.CLOUDINARY_CLOUD_NAME}`,
+}
+
+export default config
