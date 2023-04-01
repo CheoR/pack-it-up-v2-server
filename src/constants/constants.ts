b132da01700e8e2d3b72c5b3026cf72395bc9862
CheoR <cheo.roman+github@gmail.com>
2023-03-31 20:57:45 -0500

    chore: resolve prettier error: line too long

--- src/constants/constants.ts
+++ src/constants/constants.ts
@@ -1,5 +1,9 @@
-export const ACCESS_TOKEN_DURATION = process.env.ACCESS_TOKEN_DURATION as unknown as string
-export const ACCESS_TOKEN_SECRET = process.env.ACCESS_TOKEN_SECRET as unknown as string
-export const REFRESH_TOKEN_SECRET = process.env.REFRESH_TOKEN_SECRET as unknown as string
-export const REFRESH_TOKEN_DURATION = process.env.REFRESH_TOKEN_DURATION as unknown as string
+export const ACCESS_TOKEN_DURATION = process.env
+  .ACCESS_TOKEN_DURATION as unknown as string
+export const ACCESS_TOKEN_SECRET = process.env
+  .ACCESS_TOKEN_SECRET as unknown as string
+export const REFRESH_TOKEN_SECRET = process.env
+  .REFRESH_TOKEN_SECRET as unknown as string
+export const REFRESH_TOKEN_DURATION = process.env
+  .REFRESH_TOKEN_DURATION as unknown as string
 export const SALT = process.env.SALT as unknown as string
